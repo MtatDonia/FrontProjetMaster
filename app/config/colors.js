ff7408babe70552f5f1e143b8a0d04c84b7cdaa7
MtatDonia <donia8054@gmail.com>
2024-02-27 11:18:14 +0100

    déclaration des couleurs

---
+++ app/config/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+    primary: '#c0f6b6',
+    secondary: '#6c757d',
+    success: '#c0f6b6',
+    danger: '#f3989e',
+    warning: '#ffc107',
+    info: '#438E93',
+    light: '#f8f9fa',
+    dark: '#343a40',
+    white: '#fff',
+    pink: '#f3a0a0',
+    black: '#000',
+    grey: '#d3d3d3',
+    purple: '#851F56',
+};
+
+export default colors;
